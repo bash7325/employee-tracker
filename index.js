0763080df34d014bf7308a128cd7ef25ad2a364f
Brandon Ashby <bash7325@gmail.com>
2021-03-10 11:44:31 -0700

    added case switches

--- index.js
+++ index.js
@@ -25,6 +25,47 @@ function start() {
         name:"startQuestions",
         type: "list",
         message: "What would you like to do?",
-        choices: [""]
+        choices: ['View All Employees',
+        'View Employees By Department',
+        'View Employees By Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Update Employee Role',
+        'Update Employee Manager',]
 })
-}
+.then((answer) => {
+    switch (answer.action) {
+        case 'View All Employees':
+            allEmployeeSearch();
+            break;
+        
+        case 'View Employees By Department':
+            allEmployeesByDepartment();
+            break;
+
+        case 'View Employees By Manager':
+            allEmployeesByManager();
+            break;
+        
+        case 'Add Employee':
+            addEmployee();
+            break;
+
+        case 'Remove Employee':
+            removeEmployee();
+            break;
+
+        case 'Update Employee Role':
+            updateEmployeeRole();
+            break;
+
+        case 'Update Employee Manager':
+            updateEmployeeManager();
+            break;
+
+        default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+    }
+});
+};
